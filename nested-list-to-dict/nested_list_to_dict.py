ffe88e8b9aeabab605c353aec6f5e6411cd92252
gabrielbarragan <gabrielbarragan07@gmail.com>
2022-05-21 16:21:32 -0500

    se optimiza la solución utilizando list y dict comprehensions

--- nested-list-to-dict/nested_list_to_dict.py
+++ nested-list-to-dict/nested_list_to_dict.py
@@ -1,11 +1,6 @@
 def nested_list_to_dict(nested_list: list= []) -> list:
     """"""
 
-    my_list_result=[]
-    for lista in nested_list:
-        my_dict= {field[0]:field[1] for field in lista}
-        my_list_result.append(my_dict)
-        
+    my_list_result= [{field[0]:field[1] for field in lista} for lista in nested_list] 
 
     return my_list_result
-
